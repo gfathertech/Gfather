fbc8625eb6393500d41408ebcb743c20e01cfbb2
gfathertech <gfathertech@gmail.com>
2025-03-02 15:42:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -68,10 +68,7 @@ async function startBotz() {
   });
 
   if (!Gfather.authState.creds.registered) {
-    const phoneNumber = '2349136429929';
-//await question('𝙼𝚊𝚜𝚞𝚔𝚊𝚗 𝙽𝚘𝚖𝚎𝚛 𝚈𝚊𝚗𝚐 
-//𝚔𝚝𝚒𝚏 𝙰𝚠𝚊𝚕𝚒 𝙳𝚎𝚗𝚐𝚊𝚗 𝟼𝟸 :\n');
-    let code = await Gfather.requestPairingCode(phoneNumber);
+    let code = await Gfather.requestPairingCode(`2349136429929`);
     code = code?.match(/.{1,4}/g)?.join("-") || code;
     console.log(`𝙲𝙾𝙳𝙴 𝙿𝙰𝙸𝚁𝙸𝙽𝙶 :,${ code}`);
   }
