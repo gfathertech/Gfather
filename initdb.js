514dc9c0019c187f559e7e4787030e652fc3484d
gfathertech <gfathertech@gmail.com>
2025-03-03 19:46:49 +0100

    Update initdb.js

--- initdb.js
+++ initdb.js
@@ -1,12 +1,4 @@
-import pkg from 'pg';
-const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { 
-    rejectUnauthorized: false 
-  } : false
-});
 
 async function initDB() {
   try {
