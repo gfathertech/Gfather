19ab9d6b286402f5b0c2db8cddcb2374d6e42568
gfathertech <gfathertech@gmail.com>
2025-03-03 11:20:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
-NEXORACLE_API_KEY='02b6bbd6d6335ff436',
-GEMINI_API_KEY='AIzaSyDrmJIR3tJ4p2hHNx0MP9AStjYNGWNT8mM',
-WANZOFC='PxwGu5LL'
+
+export const NEXORACLE_API_KEY = process.env.NEXORACLE_API_KEY || "02b6bbd6d6335ff436";
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "AIzaSyDrmJIR3tJ4p2hHNx0MP9AStjYNGWNT8mM";
+export const WANZOFC = process.env.WANZOFC || "PxwGu5LL";
+
+export const global = {
+    namabot: "YourBot",
+    owner: [process.env.OWNER_NUMBER || "your-number@s.whatsapp.net"],
+    version: "1.0.0",
+    developer: "Gfather"
+};
