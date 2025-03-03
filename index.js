d7bae47a355ef9004613e73dbf95159c496a23b7
gfathertech <gfathertech@gmail.com>
2025-03-03 20:39:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { startBot } from "./bot.js";
 import express from "express";
 import initDB from "./initdb.js";
+import verifyDB from "./db-check.js";
 
 const app = express();
 app.get("/ping", (req, res) => res.sendStatus(200));
@@ -8,6 +9,7 @@ app.get("/ping", (req, res) => res.sendStatus(200));
 (async () => {
   try {
     await initDB();
+    await verifyDB();
     app.listen(process.env.PORT || 3000, () => {
       console.log(`✅ Server running on port ${process.env.PORT || 3000}`);
       startBot();
