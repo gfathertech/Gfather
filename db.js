52d1579824f7f710c8356663d290d374e4b007d2
gfathertech <gfathertech@gmail.com>
2025-03-03 21:47:10 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+export default new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
-export default pool; // Add default export
-export { pool }; // Keep named export
