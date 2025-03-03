e86581c89025638d3a0465e8cfbf7ad8de1745c9
gfathertech <gfathertech@gmail.com>
2025-03-03 19:32:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: process.env.NODE_ENV === 'production' ? { 
+    rejectUnauthorized: false 
+  } : false
 });
 
 import handleMessage from "./case.js";
